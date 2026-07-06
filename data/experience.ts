1e05ad2dd4d7066c65b7c832f8094231ccfd247e
Jalyn Tandy <jalynrtandy@gmail.com>
2026-07-06 00:08:37 -0500

    feat: Added experience section entries

---
+++ data/experience.ts
@@ -0,0 +1,16 @@
+  export const experiences = [
+    {
+      role: "Technology Consultant Intern - Business Platform Transformation",
+      company: "Protiviti",
+      date: "June 2026 - July 2026",
+      description: "Identified business inefficiencies",
+      tags: ["Excel", "PowerBI"]
+    },
+    {
+      role: "IT Intern",
+      company: "The City of Michigan City",
+      date: "May 2024 - July 2024",
+      description: "Troubleshot technology across the city",
+      tags: ["SQL", "Sortly"]
+    }
+  ] 
